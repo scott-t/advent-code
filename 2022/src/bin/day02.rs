c94a0b9fc09cde13cab32ac1e7d87add4aff5d92
Scott Thomas <s@sthomas.id.au>
2022-12-03 16:31:04 +1030

    Day2

--- 2022/src/bin/day02.rs
+++ 2022/src/bin/day02.rs
@@ -3,8 +3,8 @@ use std::io::{self,BufRead};
 use std::path::Path;
 
 struct Moves {
-    me: i8,
-    them: i8,
+    me: i32,
+    them: i32,
 }
 
 impl std::str::FromStr for Moves {
@@ -12,23 +12,28 @@ impl std::str::FromStr for Moves {
 
     fn from_str(pair: &str) -> Result<Self, Self::Err> {
         let p = pair.as_bytes();
-        Ok(Moves { me: ((p[2] as i8)  - ('X' as i8)) as i8, them: ((p[0] as i8)  - ('A' as i8) ) as i8 })
+        Ok(Moves { me: ((p[2] as i32)  - ('X' as i32)), them: ((p[0] as i32)  - ('A' as i32))})
     }
 }
 
 
 fn main() {
     if let Ok(lines) = read_lines("input/day02.txt") {
-        let mut score: u64 = 0;
+        let mut score1 = 0;
+        let mut score2 = 0;
         for line in lines {
             if let Ok(mv) = line {
                 let game = mv.parse::<Moves>().unwrap();
 
                 // Probably a neater way. +1 due to rock = 0 in input
-                score += ((game.me - game.them + 4) % 3 * 3 + game.me + 1) as u64;
+                score1 += (game.me - game.them + 4) % 3 * 3 + game.me + 1;
+                //println!("{} {}", (game.me * 3), (game.them + game.me + 2) % 3 + 1);
+                score2 += game.me * 3;
+                score2 += (game.them + game.me + 2) % 3 + 1;
             }
         }
-        println!("Part 1 {}", score);
+        println!("Part 1 {}", score1);
+        println!("Part 2 {}", score2);
     }
 }
 
